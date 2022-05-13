e3c455836c331863f9a7c9a14c899de6c662e4b0
sladesladeslade <spbrooks4@gmail.com>
2022-05-13 00:36:26 -0400

    first test

---
+++ 20220513_RemoteKRPCDataTest.py
@@ -0,0 +1,24 @@
+import krpc
+
+# fxn to connect to krpc server
+conn = krpc.connect(
+    name = "Test",
+    address = "192.168.1.1",
+    rpc_port = 1000,
+    stream_port = 1001)
+
+# print status of connection
+print(conn.krpc.get_status().version)
+
+# add vessel
+vessel = conn.space_center.active_vessel
+vessel.name = "TestVessel"
+
+# get flight info and print
+flight_info = vessel.flight()
+print(flight_info.mean_altitude)
+
+# add stream for data
+altitude = conn.add_stream(getattr, flight_info, "mean_altitude")
+while True:
+    print(altitude())
